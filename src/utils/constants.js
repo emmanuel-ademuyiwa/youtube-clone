541434842d84a1386208671251b9cc0b2ea5a4bc
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-07-05 22:14:40 +0100

    Styles clean up

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -38,5 +38,5 @@ export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
 export const demoChannelTitle = 'Emmanuel Ademuyiwa';
-export const demoVideoTitle = 'Build and Deploy YouTube clone in 3 Hours - Full Course | RapidAPI';
+export const demoVideoTitle = 'YouTube clone | RapidAPI';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
